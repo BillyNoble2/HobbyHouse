179002e0d02b314c56f11333214733de0504a29c
Dom <dominic.walsh45@gmail.com>
2023-03-22 18:32:14 +0000

    Changed create event to my_clubs. Populated my clubs. Re worked toolbar

---
+++ app/src/main/java/com/example/myapplication/club_view_my_clubs.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class club_view_my_clubs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.club_view_my_clubs);
+    }
+}
